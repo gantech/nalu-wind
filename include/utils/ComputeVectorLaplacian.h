577c9a1682a65dc7fa5dc50a15546c7ffe8e7a27
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2020-03-15 19:59:15 -0600

    Compute vector laplacian function

---
+++ include/utils/ComputeVectorLaplacian.h
@@ -0,0 +1,39 @@
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+//
+
+
+#ifndef COMPUTEVECTORLAPLACIAN_H
+#define COMPUTEVECTORLAPLACIAN_H
+
+#include <FieldTypeDef.h>
+
+namespace stk {
+namespace mesh {
+  class BulkData;
+  class Part;
+  typedef std::vector<Part*> PartVector;
+}
+}
+
+namespace sierra {
+namespace nalu {
+
+void compute_vector_laplacian(
+  stk::mesh::BulkData&,
+  stk::mesh::PartVector&,
+  stk::mesh::PartVector&,
+  stk::mesh::FieldBase*,
+  stk::mesh::FieldBase*,  
+  stk::mesh::FieldBase*,
+  const bool hasMeshDeformation = false );
+
+}
+}
+
+#endif /* COMPUTEVECTORLAPLACIAN_H */
