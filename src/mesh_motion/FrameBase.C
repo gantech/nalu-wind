89faef3df233c3e75c5f19e6edc366684259a88f
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2021-09-19 17:22:46 -0600

    Add a sinusoidal rotation mesh motion kernel

--- src/mesh_motion/FrameBase.C
+++ src/mesh_motion/FrameBase.C
@@ -4,6 +4,7 @@
 #include "mesh_motion/MotionDeformingInteriorKernel.h"
 #include "mesh_motion/MotionScalingKernel.h"
 #include "mesh_motion/MotionRotationKernel.h"
+#include "mesh_motion/MotionSinRotationKernel.h"
 #include "mesh_motion/MotionTranslationKernel.h"
 #include "mesh_motion/TurbineSurrogateKernel.h"
 #include "mesh_motion/MotionWavesKernel.h"
@@ -70,6 +71,8 @@ FrameBase::load(const YAML::Node& node)
           new MotionDeformingInteriorKernel(meta_, motion_def));
       else if (type == "rotation")
         motionKernels_[i].reset(new MotionRotationKernel(motion_def));
+      else if (type == "sin_rotation")
+          motionKernels_[i].reset(new MotionSinRotationKernel(motion_def));
       else if (type == "scaling")
         motionKernels_[i].reset(new MotionScalingKernel(meta_, motion_def));
       else if (type == "translation")
